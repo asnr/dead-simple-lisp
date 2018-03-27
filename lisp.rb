44c97f5ff469f76e0fa87a49027ebb45a50e66d3
asnr <asnr@users.noreply.github.com>
2018-03-27 16:45:45 +1100

    Accept code from command line arguments

--- lisp.rb
+++ lisp.rb
@@ -3,22 +3,28 @@
 require './lexer'
 require './parser'
 
-if ARGV.length != 1
-  puts 'Usage: ruby lisp.rb lisp_code_file'
-  exit 1
-end
-
-code_filename = ARGV[0]
-
-File.open(code_filename) do |code_file|
-  code = code_file.read
+def main(code)
   tokens = Lexer.new.tokenize(code)
   syntax_tree = Parser.parse(tokens)
-  puts 'Code:'
-  puts '-----'
+  puts 'Code'
+  puts '----'
   puts code
   puts ''
-  puts 'Abstract syntax tree:'
-  puts '---------------------'
+  puts 'Abstract syntax tree'
+  puts '--------------------'
   p syntax_tree
+end
+
+if ARGV.length == 1
+  code_filename = ARGV[0]
+  File.open(code_filename) do |code_file|
+    main(code_file.read)
+  end
+elsif ARGV.length == 2 && ARGV[0] == '-e'
+  code = ARGV[1]
+  main(code)
+else
+  puts 'Usage: ruby lisp.rb <lisp-code-file>'
+  puts '       ruby lisp.rb -e <lisp-code>'
+  exit 1
 end
